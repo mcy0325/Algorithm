fb4d89bea1056534a7272d7743bbf4f4970ed1ae
Mun Chae Yeong <99884555+mcy0325@users.noreply.github.com>
2024-02-14 18:59:56 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/10430. 나머지/나머지.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << (a + b) % c << "\n";
+	cout << ((a % c) + (b % c)) % c << "\n";
+	cout << (a * b) % c << "\n";
+	cout << ((a % c) * (b % c)) % c;
+}
