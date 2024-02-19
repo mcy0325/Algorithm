00a38d50fe872367dc3218621aadc9993d10a8be
Mun Chae Yeong <99884555+mcy0325@users.noreply.github.com>
2024-02-19 16:05:25 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int a, b;
+	cin >> a >> b;
+
+	if (a > b) {
+		cout << ">";
+	}
+	else if (a < b) {
+		cout << "<";
+	}
+	else {
+		cout << "==";
+	}
+}
