6ed8db749517fd8843947879ff2daede0ec38750
Mun Chae Yeong <99884555+mcy0325@users.noreply.github.com>
2024-02-19 16:15:02 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int a, b;
+
+	while(1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) {
+			break;
+		}
+		cout << a + b << "\n";
+	}
+}
