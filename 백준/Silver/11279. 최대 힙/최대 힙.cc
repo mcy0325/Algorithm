ec003538665ffe4e0104160aea49f5c190ebcc93
Moon Chae Young <99884555+mcy0325@users.noreply.github.com>
2025-07-18 18:54:14 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+	int n;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	priority_queue<int> pq;
+
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+
+		if (tmp == 0) {
+			if (pq.empty()) {
+				cout << 0 << "\n";
+			}
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(tmp);
+		}
+	}
+
+	return 0;
+}
