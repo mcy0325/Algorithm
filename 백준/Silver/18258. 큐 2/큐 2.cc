16b66e49ec7ee706f5162176f988b49f1008bb88
Moon Chae Young <99884555+mcy0325@users.noreply.github.com>
2025-07-19 15:09:49 +0900

    [Silver IV] Title: 큐 2, Time: 320 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n;
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    
+    cin >> n;
+
+    queue<int> arr;
+
+    string s;
+    int num;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+
+        if (s == "push") {
+            cin >> num;
+            arr.push(num);
+        }
+
+        else if (s == "pop") {
+            if (arr.size() == 0) {
+                ans = -1;
+                cout << ans << "\n";
+            }
+            else {
+                ans = arr.front();
+                cout << ans << "\n";
+                arr.pop();
+            }
+        }
+
+        else if (s == "size") {
+            cout << arr.size() << "\n";
+        }
+
+        else if (s == "empty") {
+            if (arr.size() == 0) {
+                ans = 1;
+                cout << ans << "\n";
+            }
+            else {
+                ans = 0;
+                cout << ans << "\n";
+            }
+        }
+
+        else if (s == "front") {
+            if (arr.size() == 0) {
+                ans = -1;
+                cout << ans << "\n";
+            }
+            else {
+                ans = arr.front();
+                cout << ans << "\n";
+            }
+        }
+
+        else if (s == "back") {
+            if (arr.size() == 0) {
+                ans = -1;
+                cout << ans << "\n";
+            }
+            else {
+                ans = arr.back();
+                cout << ans << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
