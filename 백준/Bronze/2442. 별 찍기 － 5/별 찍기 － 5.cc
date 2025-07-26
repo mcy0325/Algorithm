5418d00a93c34da9daab1a5ee320ac4fcf51bcc8
Moon Chae Young <99884555+mcy0325@users.noreply.github.com>
2025-07-26 12:59:49 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = n; i >= 1; i--) {
+		for (int j = 1; j < i; j++) {
+			cout << " ";
+		}
+		for (int j = (n - i + 1) * 2 - 1; j > 0; j--) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
